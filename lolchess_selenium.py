77fc775c4b7dd8f9e6bed9664165f939da0eedb1
sihogu <73099483+sihogu@users.noreply.github.com>
2024-04-03 14:35:27 +0900

    Add files via upload

--- lolchess_selenium.py
+++ lolchess_selenium.py
@@ -5,6 +5,15 @@ import requests
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+import re
+
+
+url = 'https://lolchess.gg'
+html = make_url(url+'/meta')
+driver = webdriver.Chrome()
+
+number = len(html.find_all('div',{'class':'css-128625v emls75t7'}))
+numbers = range(1,number)
 
 
 def make_url(x_url):
@@ -32,10 +41,6 @@ def find_deck():
     x_target_table = driver.find_elements(By.CSS_SELECTOR,'.Champions.css-1z2so7.e1mdo1l0')
     return x_target_table
 
-def first_augmenter():
-    x_target_deck_name = driver.find_element(By.XPATH,'//*[@id="content-container"]/div[2]/div[2]/div[2]/div/div[1]/div/div/div[1]/h2')
-    return x_target_deck_name
-
 
 def find_deck_name(x_html,x_deck_name_list):
     x_deck_name = x_html.find('div',{'class':'css-1s5hngw e691syl3'}).find('h2').text
@@ -48,40 +53,16 @@ def find_item(x_html,x_deck_name_list):
     return x_deck_name_list
 
 
-url = 'https://lolchess.gg'
-html = make_url(url+'/meta')
-driver = webdriver.Chrome()
-
-number = len(html.find_all('div',{'class':'css-128625v emls75t7'}))
-numbers = range(1,number)
-
-
-
-        
-def make_text():
-    deck_name_list = []
-    item_list = []
-    list_names = [deck_name_list, item_list]
-    function_names2 = [find_deck_name, find_item]
-    for i in numbers:
-        for function_name2, list_name in zip(function_names2, list_names):
-            page = find_page(i, url, html)
-            tip_page = make_url(page) #html.paser
-            
-            function_name2(tip_page, list_name)
-    return deck_name_list, item_list
-
-def make_table_image():
-    for i in numbers:
-        page = find_page(i, url, html)
-        driver.get(page)
-        sleep(0.5)
-        target = find_table()
-        target.screenshot(f'lolchess_image/table/table_{i}.png')
-        
-def make_deck_image():
-    for i in numbers:
-        driver.get(url+'/meta')
-        sleep(0.5)
-        target = find_deck()[3]
-        target.screenshot(f'lolchess_image/deck/deck_{3}.png')
+for i in numbers:
+    #table 이미지 저장
+    page = find_page(i, url, html)
+    driver.get(page)
+    sleep(0.5)
+    target = find_table()
+    target.screenshot(f'table/table_{i}.png')
+    #deck 이미지 저장
+    driver.get(url+'/meta')
+    sleep(0.5)
+    target = find_deck()[i]
+    target.screenshot(f'deck/deck_{i}.png')
+    sleep(0.5)
